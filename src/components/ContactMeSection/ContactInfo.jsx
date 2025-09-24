ff17962466fa3b1f546b866dcec7cc6721004a62
Gourav Verma <gouravverma2907@gmail.com>
2025-09-24 21:49:43 +0530

    final

--- src/components/ContactMeSection/ContactInfo.jsx
+++ src/components/ContactMeSection/ContactInfo.jsx
@@ -7,7 +7,7 @@ import SingleInfo from './SingleInfo';
 const ContactInfo = () => {
   return (
     <div className='flex flex-col gap-4 text-white'>
-      <SingleInfo text="gouravverma2907@gmail.com" Image={TfiEmail}/>
+      <SingleInfo text="gouravverma2901@gmail.com" Image={TfiEmail}/>
       <SingleInfo text="+91-7879558651" Image={FaMobileAlt}/>
       <SingleInfo text="Indore, (M.P.), India" Image={ImLocation}/>
     </div>
