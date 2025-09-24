846374106708a9d417e2fd7245dbdc1478ae8e34
Gourav Verma <gouravverma2907@gmail.com>
2025-09-24 21:45:53 +0530

    final

--- src/components/SkillSection/SubSkills.jsx
+++ src/components/SkillSection/SubSkills.jsx
@@ -4,7 +4,7 @@ const SubSkills = () => {
   return (
     <div className='border-y-2 border-lightGrey relative'>
       <div className='absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full'></div>
-      <img src="../../public/images/SDBanner.jpg" alt="Sub Skills Image" className='w-full h-4 md:h-20 lg:h-10'/>
+      <img src="./images/SDBanner.jpg" alt="Sub Skills Image" className='w-full h-4 md:h-20 lg:h-10'/>
     </div>
   )
 }
